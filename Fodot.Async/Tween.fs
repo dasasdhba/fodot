d11a15d57a2824baef0336c35338ccfb57b497b0
dasasdhba <dasasdhba@outlook.com>
2026-05-03 09:56:31 +0800

    tween: fix asTask

--- Fodot.Async/Tween.fs
+++ Fodot.Async/Tween.fs
@@ -4,8 +4,10 @@ open Godot
 
 let asTask (tween: Tween) = task {
     let! _ = tween |> GodotObject.toSignal "finished"
+    ()
 }
 
 let asTaskWith ct (tween: Tween) = task {
     let! _ = tween |> GodotObject.toSignalWith ct "finished"
+    ()
 }
