12966283c3bb066f7fadc637c2a3fcc177d309e8
dasasdhba <dasasdhba@outlook.com>
2026-05-08 17:08:02 +0800

    clear submodule

--- module/Fodot.Stage/cutscene_provider.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-@export_category("CutsceneProvider")
-@export_custom(PROPERTY_HINT_NONE, "") var in_scene : PackedScene
-@export_custom(PROPERTY_HINT_NONE, "") var out_scene : PackedScene
-@export_custom(PROPERTY_HINT_NONE, "") var reuse_in_if_out_is_null : bool = true
-
-func _get_fscripts():
-	return ["cutscene_provider"]
